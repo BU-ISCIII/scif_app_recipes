d570dc94d7cfa116f6ce3648689a49e9be2e0828
saramonzon <sara.monzon.fdz@gmail.com>
2018-08-28 14:02:50 +0200

    docker-entrypoint script working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-find /scif/apps -maxdepth 2 -name "bin" | while read in; do echo "export PATH=\${PATH}:$in";done
+find /scif/apps -maxdepth 2 -name "bin" | while read in; do echo "export PATH=\${PATH}:$in" >> /opt/path_export ;done
+source /opt/path_export
 
 exec "$@"
