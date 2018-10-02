67dc7ca08cfdaa3302c54976b9ff0a30cbbcbace
saramonzon <sara.monzon.fdz@gmail.com>
2018-10-02 14:11:56 +0200

    Finished snppipeline and outbreaker for testing. Trying unicycler

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,16 @@
 set -e
 
 find /scif/apps -maxdepth 2 -name "bin" | while read in; do echo "export PATH=\${PATH}:$in" >> /opt/path_export ;done
+find /scif/apps -maxdepth 2 -name "lib" | while read in; do echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:$in" >> /opt/ld_library_export ;done
+
 source /opt/path_export
+source /opt/ld_library_export
+
+## Custom parameters needed for some software.
+if [[ echo ${PATH} | grep -q /scif/apps/snppipeline ]];then
+	export CLASSPATH=~/scif/apps/varscan/varscan-2.3.9/varscan-2.3.9.jar:$CLASSPATH
+	export CLASSPATH=~/scif/apps/picard/picard.jar:$CLASSPATH
+	export CLASSPATH=~/scif/apps/gatk/gatk-3.8/GenomeAnalysisTK.jar:$CLASSPATH
+fi
 
 exec "$@"
